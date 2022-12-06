b2e139100941b7ea1c979bb7491d50814485e7d7
Neil Sciberras <119696729+neil-scib@users.noreply.github.com>
2022-12-06 17:49:02 +0100

    WIP - implemented MovieGrain

--- Movies.Grains/Movies.Grains/MovieGrain.cs
+++ Movies.Grains/Movies.Grains/MovieGrain.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Movies.Contracts.Models;
+using Movies.Grains.Interfaces;
+using Movies.Infrastructure.FileSystem;
+using Orleans;
+using Orleans.Runtime;
+using System;
+using System.Threading.Tasks;
 
 namespace Movies.Grains
 {
-	internal class MovieGrain
+	public class MovieGrain : Grain, IMovieGrain
 	{
+		private readonly IPersistentState<Movie> _state;
+
+		public MovieGrain(IPersistentState<Movie> state)
+		{
+			_state = state;
+		}
+
+		public Task<Movie> Get(int id) => throw new NotImplementedException();
+
+		public Task Set(Movie movie) => throw new NotImplementedException();
+		
+		
 	}
 }
