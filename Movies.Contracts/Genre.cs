33751453ef9d998eb767d9b0b9621ee67fd31827
Neil Sciberras <119696729+neil-scib@users.noreply.github.com>
2022-12-06 14:03:07 +0100

    Added movie contract type

---
+++ Movies.Contracts/Genre.cs
@@ -0,0 +1,17 @@
+﻿namespace Movies.Contracts
+{
+	public enum Genre
+	{
+		Action,
+		Adventure,
+		Comedy,
+		Crime,
+		Biography,
+		Drama,
+		History,
+		Sport,
+		Mystery,
+		Thriller,
+		SciFi
+	}
+}
---
+++ Movies.Contracts/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Movies.Contracts
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string Key { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public IEnumerable<Genre> Genres { get; set; }
+		public double Rate { get; set; }
+		public string Length { get; set; }
+		public string Image { get; set; }
+	}
+}
