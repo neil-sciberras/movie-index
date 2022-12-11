730cfa4aa58dff9a8e3433186694a9fb0bfca7cf
Neil Sciberras <119696729+neil-scib@users.noreply.github.com>
2022-12-11 14:30:19 +0100

    Coered grain clients with unit tests

---
+++ tests/Movies.Grains/Movies.Grains.Clients.Tests/QueryClientTests.cs
@@ -0,0 +1,109 @@
+﻿using Moq;
+using Movies.Contracts.Models;
+using Movies.Grains.Clients.Interfaces;
+using Movies.Grains.Interfaces;
+using NUnit.Framework;
+using Orleans;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Movies.Grains.Clients.Tests
+{
+	[TestFixture]
+	internal class QueryClientTests
+	{
+		private Mock<IGrainFactory> _grainFactoryMock;
+		private Mock<IAllMoviesGrain> _allMoviesGrainMock;
+		private Mock<IGenreFilterGrain> _genreFilterGrainMock;
+		private Mock<IMovieGrain> _movieGrainMock;
+		private Mock<ITopRatedMoviesGrain> _topRatedMoviesGrainMock;
+
+		private IQueryClient QueryClient => new QueryClient(_grainFactoryMock.Object);
+
+		[SetUp]
+		public void Setup()
+		{
+			_grainFactoryMock = new Mock<IGrainFactory>();
+			_allMoviesGrainMock = new Mock<IAllMoviesGrain>();
+			_genreFilterGrainMock = new Mock<IGenreFilterGrain>();
+			_movieGrainMock = new Mock<IMovieGrain>();
+			_topRatedMoviesGrainMock = new Mock<ITopRatedMoviesGrain>();
+		}
+
+		[Test]
+		public async Task GetAllMoviesTest()
+		{
+			// Arrange
+			_allMoviesGrainMock.Setup(m => m.GetAllMoviesAsync())
+				.ReturnsAsync(new List<Movie>()).Verifiable();
+			
+			_grainFactoryMock
+				.Setup(m => m.GetGrain<IAllMoviesGrain>(It.IsAny<string>(), null))
+				.Returns(_allMoviesGrainMock.Object);
+
+			// Act
+			var result = await QueryClient.GetAllMoviesAsync();
+
+			// Assert
+			Assert.NotNull(result);
+			_allMoviesGrainMock.Verify(m => m.GetAllMoviesAsync(), Times.Once);
+		}
+
+		[Test]
+		public async Task GetMoviesByGenreTest()
+		{
+			// Arrange
+			_genreFilterGrainMock.Setup(m => m.GetMoviesAsync(It.IsAny<Genre>()))
+				.ReturnsAsync(new List<Movie>()).Verifiable();
+			
+			_grainFactoryMock
+				.Setup(m => m.GetGrain<IGenreFilterGrain>(It.IsAny<string>(), null))
+				.Returns(_genreFilterGrainMock.Object);
+
+			// Act
+			var result = await QueryClient.GetMoviesAsync(Genre.History);
+
+			// Assert
+			Assert.NotNull(result);
+			_genreFilterGrainMock.Verify(m => m.GetMoviesAsync(It.Is<Genre>(g => g == Genre.History)), Times.Once);
+		}
+
+		[Test]
+		public async Task GetMovieTest()
+		{
+			// Arrange
+			_movieGrainMock.Setup(m => m.GetMovieAsync())
+				.ReturnsAsync(new Movie()).Verifiable();
+			
+			_grainFactoryMock
+				.Setup(m => m.GetGrain<IMovieGrain>(It.IsAny<long>(), null))
+				.Returns(_movieGrainMock.Object);
+
+			// Act
+			var result = await QueryClient.GetMovieAsync(1);
+
+			// Assert
+			Assert.NotNull(result);
+			_movieGrainMock.Verify(m => m.GetMovieAsync(), Times.Once);
+		}
+
+		[Test]
+		public async Task GetTopRatedMoviesTest()
+		{
+			// Arrange
+			_topRatedMoviesGrainMock.Setup(m => m.GetMoviesAsync(It.IsAny<int>()))
+				.ReturnsAsync(new List<Movie>()).Verifiable();
+			
+			_grainFactoryMock
+				.Setup(m => m.GetGrain<ITopRatedMoviesGrain>(It.IsAny<string>(), null))
+				.Returns(_topRatedMoviesGrainMock.Object);
+
+			// Act
+			var result = await QueryClient.GetTopRatedMoviesAsync(2);
+
+			// Assert
+			Assert.NotNull(result);
+			_topRatedMoviesGrainMock.Verify(m => m.GetMoviesAsync(2), Times.Once);
+		}
+	}
+}
---
+++ tests/Movies.Grains/Movies.Grains.Clients.Tests/UpdateClientTests.cs
@@ -0,0 +1,84 @@
+﻿using Moq;
+using Movies.Contracts.Models;
+using Movies.Grains.Clients.Interfaces;
+using Movies.Grains.Interfaces.Updates;
+using NUnit.Framework;
+using Orleans;
+using System.Threading.Tasks;
+
+namespace Movies.Grains.Clients.Tests
+{
+	[TestFixture]
+	internal class UpdateClientTests
+	{
+		private Mock<IGrainFactory> _grainFactoryMock;
+		private Mock<IAddMovieGrain> _addMovieGrainMock;
+		private Mock<IDeleteMovieGrain> _deleteMovieGrainMock;
+		private Mock<IUpdateMovieGrain> _updateMovieGrainMock;
+
+		private IUpdateClient UpdateClient => new UpdateClient(_grainFactoryMock.Object);
+
+		[SetUp]
+		public void Setup()
+		{
+			_grainFactoryMock = new Mock<IGrainFactory>();
+			_addMovieGrainMock = new Mock<IAddMovieGrain>();
+			_deleteMovieGrainMock = new Mock<IDeleteMovieGrain>();
+			_updateMovieGrainMock = new Mock<IUpdateMovieGrain>();
+		}
+
+		[Test]
+		public async Task AddMovieTest()
+		{
+			// Arrange
+			_addMovieGrainMock.Setup(m => m.AddMovieAsync(It.IsAny<NewMovie>()))
+				.ReturnsAsync(new Movie()).Verifiable();
+			_grainFactoryMock
+				.Setup(m => m.GetGrain<IAddMovieGrain>(It.IsAny<string>(), null))
+				.Returns(_addMovieGrainMock.Object);
+
+			// Act
+			var result = await UpdateClient.AddMovieAsync(new NewMovie());
+
+			// Assert
+			Assert.NotNull(result);
+			_addMovieGrainMock.Verify(m => m.AddMovieAsync(It.IsAny<NewMovie>()), Times.Once);
+		}
+
+		[Test]
+		public async Task DeleteMovieTest()
+		{
+			// Arrange
+			_deleteMovieGrainMock.Setup(m => m.DeleteMovieAsync(It.IsAny<int>()))
+				.ReturnsAsync(new Movie()).Verifiable();
+			_grainFactoryMock
+				.Setup(m => m.GetGrain<IDeleteMovieGrain>(It.IsAny<string>(), null))
+				.Returns(_deleteMovieGrainMock.Object);
+
+			// Act
+			var result = await UpdateClient.DeleteMovieAsync(1);
+
+			// Assert
+			Assert.NotNull(result);
+			_deleteMovieGrainMock.Verify(m => m.DeleteMovieAsync(1), Times.Once);
+		}
+
+		[Test]
+		public async Task UpdateMovieTest()
+		{
+			// Arrange
+			_updateMovieGrainMock.Setup(m => m.UpdateMovieAsync(It.IsAny<Movie>()))
+				.ReturnsAsync(new Movie()).Verifiable();
+			_grainFactoryMock
+				.Setup(m => m.GetGrain<IUpdateMovieGrain>(It.IsAny<string>(), null))
+				.Returns(_updateMovieGrainMock.Object);
+
+			// Act
+			var result = await UpdateClient.UpdateMovieAsync(new Movie());
+
+			// Assert
+			Assert.NotNull(result);
+			_updateMovieGrainMock.Verify(m => m.UpdateMovieAsync(It.IsAny<Movie>()), Times.Once);
+		}
+	}
+}
