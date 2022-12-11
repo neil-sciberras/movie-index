faabeb8a3f97f9219a10fbea63a4d50c8db73ed1
Neil Sciberras <119696729+neil-scib@users.noreply.github.com>
2022-12-11 13:53:46 +0100

    Extracted some common code into a base class

--- Movies.Grains/Movies.Grains/Updates/DeleteMovieGrain.cs
+++ Movies.Grains/Movies.Grains/Updates/DeleteMovieGrain.cs
@@ -1,37 +1,20 @@
-﻿using Movies.Contracts.Grains;
-using Movies.Contracts.Models;
-using Movies.Grains.Interfaces;
+﻿using Movies.Contracts.Models;
 using Movies.Grains.Interfaces.Updates;
 using Orleans;
 using System.Threading.Tasks;
 
 namespace Movies.Grains.Updates
 {
-	public class DeleteMovieGrain : Grain, IDeleteMovieGrain
+	public class DeleteMovieGrain : UpdateOrDeleteGrainBase, IDeleteMovieGrain
 	{
-		private readonly IGrainFactory _grainFactory;
-
-		public DeleteMovieGrain(IGrainFactory grainFactory)
+		public DeleteMovieGrain(IGrainFactory grainFactory) : base(grainFactory)
 		{
-			_grainFactory = grainFactory;
 		}
 
 		public async Task<Movie> DeleteMovieAsync(int id)
 		{
-			var movieGrain = _grainFactory.GetGrain<IMovieGrain>(id);
-			var movie = await movieGrain.GetMovieAsync();
-
-			if (movie == null)
-			{
-				return null;
-			}
-
-			var allMoviesGrain = _grainFactory.GetGrain<IAllMoviesGrain>(GrainIds.AllMoviesGrainId);
-
-			await movieGrain.DeleteMovieAsync();
-			await allMoviesGrain.ResetAsync();
-
-			return movie;
+			return await EditExistingMovieAsync(id, editFunc: async movieGrain => 
+				await movieGrain.DeleteMovieAsync());
 		}
 	}
 }
--- Movies.Grains/Movies.Grains/Updates/UpdateMovieGrain.cs
+++ Movies.Grains/Movies.Grains/Updates/UpdateMovieGrain.cs
@@ -1,40 +1,23 @@
-﻿using Movies.Contracts.Grains;
-using Movies.Contracts.Models;
+﻿using Movies.Contracts.Models;
 using Movies.Grains.ContractExtensions;
-using Movies.Grains.Interfaces;
 using Movies.Grains.Interfaces.Updates;
 using Orleans;
 using System.Threading.Tasks;
 
 namespace Movies.Grains.Updates
 {
-	public class UpdateMovieGrain : Grain, IUpdateMovieGrain
+	public class UpdateMovieGrain : UpdateOrDeleteGrainBase, IUpdateMovieGrain
 	{
-		private readonly IGrainFactory _grainFactory;
-
-		public UpdateMovieGrain(IGrainFactory grainFactory)
-		{
-			_grainFactory = grainFactory;
-		}
+		public UpdateMovieGrain(IGrainFactory grainFactory) : base(grainFactory) { }
 
 		public async Task<Movie> UpdateMovieAsync(Movie movieUpdate)
 		{
-			var movieGrain = _grainFactory.GetGrain<IMovieGrain>(movieUpdate.Id);
-			var currentMovie = await movieGrain.GetMovieAsync();
-
-			if (currentMovie == null)
-			{
-				return null;
-			}
-
-			var updatedMovie = currentMovie.UpdateMovie(movieUpdate);
-
-			var allMovieGrain = _grainFactory.GetGrain<IAllMoviesGrain>(GrainIds.AllMoviesGrainId);
-
-			await movieGrain.SetMovieAsync(updatedMovie);
-			await allMovieGrain.ResetAsync();
-
-			return updatedMovie;
+			return await EditExistingMovieAsync(movieUpdate.Id, editFunc: async movieGrain =>
+				{
+					var currentMovie = await movieGrain.GetMovieAsync();
+					var updatedMovie = currentMovie.UpdateMovie(movieUpdate);
+					await movieGrain.SetMovieAsync(updatedMovie);
+				});
 		}
 	}
 }
